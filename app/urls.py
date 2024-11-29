1dfbe08e9cc56a6fdc1c467f687597ef4fe05d4c
yadhukrishnx <yadhukrishnx@gmail.com>
2024-11-29 18:24:38 +0530

    feat(login/auth) : OTP Login using Email Added

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
+    path("register/", views.signup, name="register"),
+    path("verify-email/<slug:username>", views.verify_email, name="verify-email"),
+    path("resend-otp", views.resend_otp, name="resend-otp"),
+    path("login/", views.signin, name="signin")
 ]
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,3 +120,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'app.CustomUser'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
